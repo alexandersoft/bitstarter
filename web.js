0333b2c66f84c7334d4758a1db2ee078d5aa8272
Alexander Zaika <alexandersoftdev@gmail.com>
2013-07-10 19:20:04 +0000

    Modified index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
+
+var b = fs.readFileSync('index.html');
+
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(b.toString());
 });
 
 var port = process.env.PORT || 5000;
